9f8ae8540b9d1295619322a2eea2d258205aaf82
AshishMittal11 <41893308+AshishMittal11@users.noreply.github.com>
2022-01-23 12:39:51 +0530

    Add Swagger

--- StudentSvc.Api/Startup.cs
+++ StudentSvc.Api/Startup.cs
@@ -43,6 +43,7 @@ namespace StudentSvc.Api
                                       builder.AllowAnyHeader();
                                   });
             });
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -67,6 +68,20 @@ namespace StudentSvc.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.  
+            app.UseSwagger(c =>
+            {
+                c.SerializeAsV2 = true;
+            });
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),  
+            // specifying the Swagger JSON endpoint.  
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
             });
         }
     }
