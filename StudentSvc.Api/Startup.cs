2917010a15a4fe5454cd75801f77ffeebc1cfef5
AshishMittal11 <41893308+AshishMittal11@users.noreply.github.com>
2022-01-23 12:47:58 +0530

    Swagger UI Added

--- StudentSvc.Api/Startup.cs
+++ StudentSvc.Api/Startup.cs
@@ -80,7 +80,7 @@ namespace StudentSvc.Api
             // specifying the Swagger JSON endpoint.  
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Student Service");
                 c.RoutePrefix = string.Empty;
             });
         }
